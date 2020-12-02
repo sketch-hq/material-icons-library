3bf16b8b015297bdcb64d3a93cc8c3cb166a120a
Christian Klotz <hello@christianklotz.co.uk>
2020-12-02 16:26:19 +0000

    Tidy up toFile

--- src/to-file/index.ts
+++ src/to-file/index.ts
@@ -1,5 +1,3 @@
-const fs = require("fs");
-const os = require("os");
 const path = require("path");
 
 import * as Zip from "adm-zip";
@@ -9,12 +7,11 @@ const toFile = async (
   contents: FileFormat.Contents,
   filepath: string
 ): Promise<void> => {
-  await new Promise((resolve): void => {
-    const sketch = new Zip()
+  await new Promise((resolve, reject): void => {
+    const sketch = new Zip();
 
     // Write pages first and use the resulting paths for the file
     // references that are stored within the main document.json.
-
     const refs = contents.document.pages.map(
       (page): FileFormat.FileRef => {
         const p = JSON.stringify(page);
@@ -35,21 +32,29 @@ const toFile = async (
     // Write root level JSON data for document, user and meta data.
     const data = {
       document: JSON.stringify(<FileFormat.Document>{
-      ...contents.document,
-      pages: refs}),
+        ...contents.document,
+        pages: refs,
+      }),
       user: JSON.stringify(contents.user),
       meta: JSON.stringify(contents.meta),
-    }
+    };
 
     Object.entries(data).map(([key, val]) => {
-      sketch.addFile(`${key}.json`, Buffer.alloc(val.length, val), `${key} data`)
-    })
+      sketch.addFile(
+        `${key}.json`,
+        Buffer.alloc(val.length, val),
+        `${key} data`
+      );
+    });
 
-    sketch.writeZip(filepath)
-    resolve(true);
+    sketch.writeZip(filepath, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(null);
+    });
   });
 };
-
-console.log(fs);
 
 export { toFile };
