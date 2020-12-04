30606a2fa3795ce6932e3b74755bf2ca3488ee99
Ale M <bomberstudios@gmail.com>
2020-12-04 09:57:04 +0100

    Prettier to to-file

--- src/to-file/index.ts
+++ src/to-file/index.ts
@@ -1,33 +1,33 @@
-const path = require("path");
+const path = require('path')
 
-import * as Zip from "adm-zip";
-import FileFormat from "@sketch-hq/sketch-file-format-ts";
+import * as Zip from 'adm-zip'
+import FileFormat from '@sketch-hq/sketch-file-format-ts'
 
 const toFile = async (
   contents: FileFormat.Contents,
   filepath: string
 ): Promise<void> => {
   await new Promise((resolve, reject): void => {
-    const sketch = new Zip();
+    const sketch = new Zip()
 
     // Write pages first and use the resulting paths for the file
     // references that are stored within the main document.json.
     const refs = contents.document.pages.map(
       (page): FileFormat.FileRef => {
-        const p = JSON.stringify(page);
+        const p = JSON.stringify(page)
         sketch.addFile(
-          path.join("pages", `${page.do_objectID}.json`),
+          path.join('pages', `${page.do_objectID}.json`),
           Buffer.alloc(p.length, p),
           `page data for: ${page.name}`
-        );
+        )
 
         return {
-          _class: "MSJSONFileReference",
-          _ref_class: "MSImmutablePage",
+          _class: 'MSJSONFileReference',
+          _ref_class: 'MSImmutablePage',
           _ref: `pages/${page.do_objectID}`,
-        };
+        }
       }
-    );
+    )
 
     // Write root level JSON data for document, user and meta data.
     const data = {
@@ -37,24 +37,24 @@ const toFile = async (
       }),
       user: JSON.stringify(contents.user),
       meta: JSON.stringify(contents.meta),
-    };
+    }
 
     Object.entries(data).map(([key, val]) => {
       sketch.addFile(
         `${key}.json`,
         Buffer.alloc(val.length, val),
         `${key} data`
-      );
-    });
+      )
+    })
 
-    sketch.writeZip(filepath, (err) => {
+    sketch.writeZip(filepath, err => {
       if (err) {
-        reject(err);
-        return;
+        reject(err)
+        return
       }
-      resolve(null);
-    });
-  });
-};
+      resolve(null)
+    })
+  })
+}
 
-export { toFile };
+export { toFile }
