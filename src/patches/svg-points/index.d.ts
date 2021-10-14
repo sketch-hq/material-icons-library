942efb19f317c1eecb4351c74702135a6f07ea03
Ale M <bomberstudios@gmail.com>
2021-10-14 22:55:18 +0200

    prettier patch

--- src/patches/svg-points/index.d.ts
+++ src/patches/svg-points/index.d.ts
@@ -1,109 +1,117 @@
-declare module "svg-points" {
+declare module 'svg-points' {
   interface CurveArc {
-    type: "arc";
-    rx: number;
-    ry: number;
-    xAxisRotation: number;
-    largeArcFlag: number;
-    sweepFlag: number;
+    type: 'arc'
+    rx: number
+    ry: number
+    xAxisRotation: number
+    largeArcFlag: number
+    sweepFlag: number
   }
 
   interface CurveCubic {
-    type: "cubic";
-    x1: number;
-    y1: number;
-    x2: number;
-    y2: number;
+    type: 'cubic'
+    x1: number
+    y1: number
+    x2: number
+    y2: number
   }
 
   interface CurveQuadratic {
-    type: "quadratic";
-    x1: number;
-    y1: number;
+    type: 'quadratic'
+    x1: number
+    y1: number
   }
 
-  type Curve = CurveCubic | CurveQuadratic | CurveArc;
+  type Curve = CurveCubic | CurveQuadratic | CurveArc
 
   interface Point {
-    x: number;
-    y: number;
-    moveTo?: true;
-    curve?: Curve;
+    x: number
+    y: number
+    moveTo?: true
+    curve?: Curve
   }
 
   interface CycleAttrs {
-    cx: number;
-    cy: number;
-    r: number;
+    cx: number
+    cy: number
+    r: number
   }
 
   interface Circle extends CycleAttrs {
-    type: "circle";
+    type: 'circle'
   }
 
   interface EllipseAttrs {
-    cx: number;
-    cy: number;
-    rx: number;
-    ry: number;
+    cx: number
+    cy: number
+    rx: number
+    ry: number
   }
 
   interface Ellipse extends EllipseAttrs {
-    type: "ellipse";
+    type: 'ellipse'
   }
 
   interface LineAttrs {
-    x1: number;
-    x2: number;
-    y1: number;
-    y2: number;
+    x1: number
+    x2: number
+    y1: number
+    y2: number
   }
 
   interface Line extends LineAttrs {
-    type: "line";
+    type: 'line'
   }
 
   interface PathAttrs {
-    d: string;
+    d: string
   }
 
   interface Path extends PathAttrs {
-    type: "path";
+    type: 'path'
   }
 
   interface WithPoints {
-    points: string;
+    points: string
   }
 
   interface Polygon extends WithPoints {
-    type: "polygon";
+    type: 'polygon'
   }
 
   interface Polyline extends WithPoints {
-    type: "polyline";
+    type: 'polyline'
   }
 
   interface RectAttrs {
-    height: number;
-    width: number;
-    x: number;
-    y: number;
-    rx?: number;
-    ry?: number;
+    height: number
+    width: number
+    x: number
+    y: number
+    rx?: number
+    ry?: number
   }
 
   interface Rect extends RectAttrs {
-    type: "rect";
+    type: 'rect'
   }
 
   interface Group {
-    type: "g";
-    shapes: ShapeType[];
+    type: 'g'
+    shapes: ShapeType[]
   }
 
-  type ShapeType = Group | Circle | Ellipse | Line | Rect | Polygon | Polyline | Path;
+  type ShapeType =
+    | Group
+    | Circle
+    | Ellipse
+    | Line
+    | Rect
+    | Polygon
+    | Polyline
+    | Path
 
-  export function toPath(shapeOrPoints: ShapeType | Point[]): string;
+  export function toPath(shapeOrPoints: ShapeType | Point[]): string
 
-  export function toPoints(shape: ShapeType): Point[];
+  export function toPoints(shape: ShapeType): Point[]
 }
